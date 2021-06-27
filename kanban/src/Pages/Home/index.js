17373b1956499567cbe0033df48441af46b0881f
Levi-Teixeira <leviteixeira1999@gmail.com>
2021-06-27 01:02:18 -0300

    Added new column logic
    Adicionada lógica para adição de novas colunas e removidos alguns warnings da aplicação.

--- kanban/src/Pages/Home/index.js
+++ kanban/src/Pages/Home/index.js
@@ -1,15 +1,25 @@
 import { Main, Button } from "./styles";
-import {React,useEffect, useState, useContext} from "react";
+import {React,useContext} from "react";
 import background from '../../Assets/background.svg';
 import Columns from "../../Components/Column";
 import {FiPlus} from 'react-icons/fi';
 import { ListContext } from "../../Provider/provider";
-import { Assignment, Tag } from "../../Components/Column/styles";
+
 
 const Home = () =>{
 
-    const {columns} = useContext(ListContext);
+    const {columns, setColumns} = useContext(ListContext);
     
+    const addColumn = () =>{
+        const obj = ({...columns, columns:
+        {
+        title: 'New Column',
+        color: '#154c79', 
+        chores:[],
+        },
+        })
+        setColumns(Object.values(obj))
+    }
 
     return(
         
@@ -17,13 +27,13 @@ const Home = () =>{
             <h1>Kanban do projeto</h1>
             <div id='container'>
                 <Columns/>
-                <Button>
+                <Button onClick={addColumn}>
                     <FiPlus/>
                     <p>Adicionar outra lista</p>
                 </Button>     
             </div>    
                     
-            <img src={background}/>
+            <img src={background} alt=''/>
         </Main>
     )
 }
